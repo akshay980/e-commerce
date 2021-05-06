8023c8d93add982ded709a2f930a7278bf82efc0
Akshay <akshaypandya980@gmail.com>
2021-05-06 22:49:00 +0530

    first

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div classname="home">
+            <p>I m Home</p>
+        </div>
+    )
+}
+
+export default Home;
